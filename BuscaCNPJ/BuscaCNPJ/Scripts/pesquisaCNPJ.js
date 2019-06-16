efb4f14fafcd931ab28c122d7ef6b4434260938c
gabrielamnz <gmnzfranco@gmail.com>
2019-06-16 20:57:53 -0300

    Pesquisa CNPJ funcionando

---
+++ BuscaCNPJ/BuscaCNPJ/Scripts/pesquisaCNPJ.js
@@ -0,0 +1,29 @@
+﻿function pesquisaCNPJ() {
+    var cnpj = $("#cnpj").val().replace(/\D/g, '');
+
+    if (cnpj != null) {
+        $.ajax(
+            {
+                url: 'https://www.receitaws.com.br/v1/cnpj/' + cnpj,
+                method: 'GET',
+                dataType: 'jsonp',
+
+                complete: function (dados) {
+                    response = dados.responseJSON;
+                    if (response.status == 'OK') {
+                        $('#razaosocial').val(response.nome);
+                        $('#fantasia').val(response.fantasia);
+                    }
+                    else {
+                        //limpa_formulario();
+                        alert(response.message);
+                    }
+                }
+            });
+    }
+    else {
+        //Cnpj pesquisado nao foi encontrado
+        //limpa_formulario();
+        alert("CNPJ não encontrado.");
+    }
+};
